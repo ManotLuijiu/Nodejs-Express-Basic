38105f2b56df82f9f71942086208b010cd60ad8f
Manot Luijiu <manot.luijiu@gmail.com>
2021-05-15 15:47:26 +0700

    delete node_modules

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const _ = require('lodash');
+const url = require('url');
+const myURL = new URL('https://means-business.info');
 
 const items = [1, [2, [3, [4]]]];
 
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log(myURL);
